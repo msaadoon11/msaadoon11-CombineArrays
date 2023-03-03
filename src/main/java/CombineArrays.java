a5c9341e84d29244daea3e9f505e8e4d34c0e346
msaadoon11 <mustafa.saadoon@gmail.com>
2023-03-03 00:26:38 +0000

    DONE!

--- src/main/java/CombineArrays.java
+++ src/main/java/CombineArrays.java
@@ -8,7 +8,21 @@ public class CombineArrays {
      * @return an array containing the contents of arr1 followed by the contents of arr2.
      */
     public int[] combine(int[] arr1, int[] arr2){
-
-        return null;
+        // int a1 = arr1.length;
+        // int a2 = arr2.length;
+        // int res = a1+a2;        // you can use these variables assignment to make it easier writing the for loops, but not necessary. 
+        int arrs = (arr1.length + arr2.length);
+        int[] res = new int[arrs];
+        
+        for(int i=0; i<arr1.length; i++){
+            res[i] = arr1[i];
+        }
+        for(int i=0; i<arr2.length; i++){
+            res[arr1.length + i] = arr2[i];
+        }
+        for(int i=0; i<arrs; i++){
+            
+        }
+        return res;
     }
 }
